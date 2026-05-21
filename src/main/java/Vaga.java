45e054976e370d8bae11a60287460d655d8de76b
Gustavo Mazeto Pasquini <gustavopasquini2006@hotmail.com>
2026-05-21 10:51:21 -0300

    Exercício 07

--- src/main/java/Vaga.java
+++ src/main/java/Vaga.java
@@ -7,17 +7,12 @@ public class Vaga {
     private boolean coberta;
     private boolean ocupada;
 
-    private String nomeClienteReserva;
-    private LocalDateTime horarioReserva;
 
-    public Vaga(String codigo, String setor, boolean coberta, boolean ocupada,
-                String nomeClienteReserva, LocalDateTime horarioReserva) {
+    public Vaga(String codigo, String setor, boolean coberta, boolean ocupada) {
         this.codigo = codigo;
         this.setor = setor;
         this.coberta = coberta;
         this.ocupada = ocupada;
-        this.nomeClienteReserva = nomeClienteReserva;
-        this.horarioReserva = horarioReserva;
     }
 
     public String descreverStatus() {
@@ -52,11 +47,4 @@ public class Vaga {
         return ocupada;
     }
 
-    public String getNomeClienteReserva() {
-        return nomeClienteReserva;
-    }
-
-    public LocalDateTime getHorarioReserva() {
-        return horarioReserva;
-    }
 }
