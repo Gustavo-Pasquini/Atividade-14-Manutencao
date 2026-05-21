6869fad21650ac4a2a16ac392fc53bcbae7fecf9
Gustavo Mazeto Pasquini <gustavopasquini2006@hotmail.com>
2026-05-21 10:56:09 -0300

    Exercício 08

--- src/main/java/Estacionamento.java
+++ src/main/java/Estacionamento.java
@@ -2,36 +2,23 @@ public class Estacionamento {
 
     private String nome;
     private String cnpj;
-
-    private String rua;
-    private String numero;
-    private String bairro;
-    private String cidade;
-    private String estado;
-    private String cep;
+    private Endereco endereco;
 
     public Estacionamento(String nome, String cnpj, String rua, String numero,
                           String bairro, String cidade, String estado, String cep) {
         this.nome = nome;
         this.cnpj = cnpj;
-        this.rua = rua;
-        this.numero = numero;
-        this.bairro = bairro;
-        this.cidade = cidade;
-        this.estado = estado;
-        this.cep = cep;
+        this.endereco = new Endereco(rua, numero, bairro, cidade, estado, cep);
     }
 
     public void imprimirDados() {
         System.out.println("Estacionamento: " + nome);
         System.out.println("CNPJ: " + cnpj);
-        System.out.println("Endereço: " + rua + ", " + numero + " - " + bairro);
-        System.out.println("Cidade: " + cidade + " - " + estado);
-        System.out.println("CEP: " + cep);
+        endereco.imprimirDados();
     }
 
     public String getEnderecoCompleto() {
-        return rua + ", " + numero + " - " + bairro + ", " + cidade + " - " + estado + ", " + cep;
+        return endereco.getEnderecoCompleto();
     }
 
     public String getNome() {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -38,9 +38,7 @@ public class Main {
                 "A12",
                 "Setor Azul",
                 true,
-                true,
-                "Mariana Souza",
-                LocalDateTime.of(2026, 6, 20, 9, 0));
+                true);
 
         TicketEstacionamento ticket = new TicketEstacionamento(
                 "TCK-1001",
@@ -50,7 +48,7 @@ public class Main {
                 LocalDateTime.of(2026, 6, 20, 9, 30),
                 LocalDateTime.of(2026, 6, 20, 12, 15)
                 );
-        ReservaVaga reserva = new ReservaVaga(vaga, cliente);
+        ReservaVaga reserva = new ReservaVaga(vaga, cliente, "Mariana Souza", LocalDateTime.of(2026, 6, 20, 9, 0));
         RegistroPagamento pagamento = new RegistroPagamento(ticket, cliente, new BigDecimal("48.5"));
         OperacaoEstacionamento operacao = new OperacaoEstacionamento();
         CentralClientes centralClientes = new CentralClientes();
